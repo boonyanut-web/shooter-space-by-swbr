fc0a9c0d6e6fd08d266b6ec1062efa84d5652211
boonyanut-web <boonyanut@lasalleschool.ac.th>
2026-03-10 14:19:33 +0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,130 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Rocket":
+            case "Rocket0":return img`
+. . . . . . 9 9 . . . . . . 
+. . . . . 9 1 1 9 . . . . . 
+. . . . 9 1 1 1 1 9 . . . . 
+. . . . 1 1 6 a 1 1 . . . . 
+. . . . 1 6 c c a 1 . . . . 
+. . . . 1 1 c c 1 1 . . . . 
+. . . 1 1 1 1 1 1 1 1 . . . 
+. . . 9 6 1 1 1 1 a 9 . . . 
+. . 9 6 f 1 1 1 1 f a 9 . . 
+. . 9 f f 1 1 1 1 f f 9 . . 
+. 9 6 f f 1 1 1 1 f f a 9 . 
+. 9 f f f 1 1 1 1 f f f 9 . 
+9 6 f f f 1 1 1 1 f f f a 9 
+9 6 f . f f f f f f . f a 9 
+d . . . . a a a a . . . . 9 
+. . . . . 4 5 5 4 . . . . . 
+. . . . 4 . 4 4 . 4 . . . . 
+. . . 4 . 5 4 5 5 . 4 . . . 
+. . . . . . . 4 . . . . . . 
+`;
+            case "Satellite":
+            case "Satellite0":return img`
+.........bb.........
+.........bb.........
+bbbbbb...cc...bbbbbb
+ccbbbcb.b11b.bcbbbbc
+ccccccccd11dcccccccc
+cbbbbbc.d11d.cbbbbbc
+cccccc..b71b..cccccc
+........d11d........
+.........77.........
+........bccb........
+.......bccccb.......
+.......cccccc.......
+....................
+....................
+....................
+....................
+`;
+            case "UFO":
+            case "UFO0":return img`
+. . . . . . . 9 9 9 9 9 9 9 . . . . . . . 
+. . . . . . 9 9 9 9 9 1 1 9 9 . . . . . . 
+. . . . . 9 9 9 9 9 9 1 1 1 9 9 . . . . . 
+. . . . . 9 9 9 9 9 9 9 9 1 1 9 . . . . . 
+. . . . . 9 8 9 9 9 9 9 9 9 1 9 . . . . . 
+. . 4 4 4 9 9 8 8 9 9 9 9 9 9 9 4 4 4 . . 
+e 4 4 5 4 4 9 9 9 9 9 9 9 9 9 4 4 5 4 4 e 
+2 e 4 4 4 4 4 4 4 9 9 9 4 4 4 4 4 4 4 e 2 
+. 2 e 4 4 4 5 4 4 4 4 4 4 4 5 4 4 4 e 2 . 
+. 5 2 2 e 4 4 4 4 4 4 4 4 4 4 4 e 2 2 5 . 
+. . . 5 2 2 2 2 2 2 2 2 2 2 2 2 2 5 . . . 
+. . . . . . . . . . . . . . . . . . . . . 
+`;
+            case "Stealth":
+            case "Stealth0":return img`
+. . . . . 2 2 . . . . . 
+. . . 2 9 . . 9 2 . . . 
+. . . . 2 9 9 2 . . . . 
+. . . . f f f f . . . . 
+. . . 8 8 1 f 8 8 . . . 
+2 . . f 1 5 5 5 f . . 2 
+. 2 2 2 4 5 5 4 2 2 2 . 
+4 5 4 4 4 5 5 4 4 4 5 4 
+. 4 4 4 2 5 5 2 4 4 4 . 
+. 4 5 4 2 5 5 2 4 5 4 . 
+. . 4 4 2 5 5 2 4 4 . . 
+. . . 5 2 5 5 2 5 . . . 
+. . . 4 2 5 5 2 4 . . . 
+. . . . 5 5 5 5 . . . . 
+. . . . . 6 6 . . . . . 
+. . . . . 5 5 . . . . . 
+`;
+            case "Blue_Rocket":
+            case "Blue Rocket":return img`
+.......99.......
+.......99.......
+......6666......
+......9969......
+.....966666.....
+.....666666.....
+.....699666.....
+44...699666...44
+96...669666...66
+9666.669666..696
+6666.699666.6666
+96d66696666.6d96
+96d.669966666d96
+96d..669666..d96
+66d..699666..d66
+9dd..669666..dd6
+.......66.......
+......6999......
+.....969699.....
+.....666666.....
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
